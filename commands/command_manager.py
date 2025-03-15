df1dc1f8e33bfe7ef1142561380209515557e342
aofekiko <aofekiko@gmail.com>
2025-03-15 03:36:42 +0200

    Implement paging for stats command

--- commands/command_manager.py
+++ commands/command_manager.py
@@ -25,22 +25,35 @@ class CommandManager(metaclass=SingletonMeta):
 @apc.describe(days="The number of days back to collect data from")
 @apc.rename(days='days')
 async def stats(interaction: discord.Interaction, days: int):
-        messages = [message async for message in interaction.channel.history(after=(datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days)))]
+        LIMIT = 100
+        await interaction.response.defer()
+        msg = await interaction.followup.send(content="Getting all old messages, this might take a while...", wait=True)
+        afterdate = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days)
         histogram = np.zeros(days)
-        for message in messages:
-             if (message.author.id == CommandManager().discordClient.user.id and message.embeds):
-                histogram[(datetime.datetime.now(datetime.timezone.utc) - message.created_at).days] += 1
+        oldestmessagedate = datetime.datetime.now(datetime.timezone.utc)
+        while oldestmessagedate > afterdate:
+            print(f"Getting {LIMIT} messages for stats")
+            messages = [message async for message in interaction.channel.history(after=afterdate, before=oldestmessagedate, oldest_first=False, limit=LIMIT)]
+            if (len(messages) == 0):
+                break
+            for message in messages:
+                oldestmessagedate = message.created_at
+                if message.created_at < afterdate:
+                  break
+                if (message.author.id == CommandManager().discordClient.user.id and message.embeds):
+                    histogram[(datetime.datetime.now(datetime.timezone.utc) - message.created_at).days] += 1
         dates = [datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(day) for day in range(days)]
         x = np.asarray(dates, dtype='datetime64[s]')
         plt.plot(x, histogram, linewidth=0.7)
         plt.gca().tick_params(axis='x', labelrotation=45)
+        plt.gca().set_ylim(ymin=0)
         plt.tight_layout()
+        plt.title(label=f"OSINT activity in the last {days} days")
         try:
-            plt.savefig('media/fig.png')
+            plt.savefig('media/fig.png', bbox_inches='tight')
             picture = discord.File('media/fig.png')
-            await interaction.response.send_message(file=picture)
+            await msg.edit(attachments=[picture], content="")
         finally:
              os.remove('media/fig.png')
-
 
 
